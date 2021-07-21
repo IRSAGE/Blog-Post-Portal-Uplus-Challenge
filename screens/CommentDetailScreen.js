41aae646be742c086cc0fb45db107dea9751300f
IR~SAGE <sibo.egide98@gmail.com>
2021-07-21 15:47:56 +0200

    Retriving Single Comment Info

--- screens/CommentDetailScreen.js
+++ screens/CommentDetailScreen.js
@@ -10,25 +10,64 @@ const CommentDetailScreen = ({ route, navigation }) => {
     const [loading, setLoading] = useState(false);
 
 
+ useEffect(() => {
+   setLoading(true);
+   axios
+     .get(`/comments/${JSON.stringify(commentId)}`)
+     .then((response) => {
+       setComment(response.data);
+       setLoading(false);
+       console.log(response.data);
+     })
+     .catch(function (error) {
+       alert("Something went Wrong");
+       console.log(error);
+     });
+ }, []);
 
-
-
-
-
-
+    
 if (loading)
-  return <LoadingScreen text={"Retriving Post Information ... Please wait"} />;
+  return <LoadingScreen text={"Retriving Comment's Information ... Please wait"} />;
 
-  return (
-    <ScrollView>
-      <View style={styles.header}>
-        <Avatar.Image size={74} source={require("../assets/postIcon.png")} />
-        <Text style={styles.headerText}>
-          Comment With Id:{commentId} Details
-        </Text>
+    return (
+      <View style={styles.screen}>
+        <ScrollView>
+          <View style={styles.header}>
+            <Avatar.Image
+              size={74}
+              source={require("../assets/postIcon.png")}
+            />
+            <Text style={styles.headerText}>
+              Comment With Id:{commentId} Details
+            </Text>
+          </View>
+          <View style={styles.TextContainer}>
+            <DetailText>For Post With Id : {comment.postId}</DetailText>
+            <DetailText>Name: {comment.name}</DetailText>
+            <DetailText> Creator: {comment.email}</DetailText>
+            <DetailText> {comment.body}</DetailText>
+              <Button
+                mode="contained"
+                color={"#f08e25"}
+                labelStyle={{ color: "white", fontSize: 15 }}
+                style={styles.btn}
+                onPress={() => {}}
+              >
+                Delete Comment
+              </Button>
+              <Button
+                mode="contained"
+                color={"#f08e25"}
+                labelStyle={{ color: "white", fontSize: 15 }}
+                style={styles.btn}
+                onPress={() => {}}
+              >
+                Update Comment
+              </Button>
+          </View>
+        </ScrollView>
       </View>
-    </ScrollView>
-  );
+    );
 };
 
 const styles = StyleSheet.create({
@@ -50,10 +89,6 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 20,
     fontWeight: "bold",
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
   },
 });
 
